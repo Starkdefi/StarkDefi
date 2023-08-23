a9363cfd4681ed6695c162389755457feb7ce6c6
Iddriss Raaj <idd.iraaj@gmail.com>
2023-08-23 18:39:19 +0000

    configure hardhat

---
+++ hardhat.config.ts
@@ -0,0 +1,74 @@
+import { HardhatUserConfig } from "hardhat/types";
+import "@shardlabs/starknet-hardhat-plugin";
+import "@nomiclabs/hardhat-ethers";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+const config: HardhatUserConfig = {
+    solidity: "0.6.12",
+    starknet: {
+        dockerizedVersion: "0.11.2", // alternatively choose one of the two venv options below
+        // uses (my-venv) defined by `python -m venv path/to/my-venv`
+        // venv: "path/to/my-venv",
+
+        // Instead of using the dockerized Scarb, uses the command you provide here
+        // Can be a path or a resolvable command
+        scarbCommand: "scarb",
+
+        // uses the currently active Python environment (hopefully with available Starknet commands!)
+        // venv: "active",
+        recompile: false,
+
+        // the directory containing Cairo 1 compiler binaries
+        cairo1BinDir: "/Users/iddrissraaj/Documents/Workspace/Misc/cairo/target/release/cairo-language-server",
+        network: "integrated-devnet",
+        wallets: {
+            OpenZeppelin: {
+                accountName: "OpenZeppelin",
+                modulePath: "starkware.starknet.wallets.open_zeppelin.OpenZeppelinAccount",
+                accountPath: "~/.starknet_accounts"
+            }
+        }
+    },
+    networks: {
+        devnet: {
+            url: "http://127.0.0.1:5050"
+        },
+        integration: {
+            url: "https://external.integration.starknet.io"
+        },
+        integratedDevnet: {
+            url: "http://127.0.0.1:5050",
+            // venv: "active",
+            // dockerizedVersion: "<DEVNET_VERSION>",
+            args: [
+                // Uncomment the lines below to activate Devnet features in your integrated-devnet instance
+                // Read about Devnet options here: https://0xSpaceShard.github.io/starknet-devnet/docs/guide/run
+                //
+                // *Account predeployment*
+                "--seed",
+                "42"
+                // "--accounts",
+                // "1",
+                // "--initial-balance", <VALUE>
+                //
+                // *Forking*
+                // "--fork-network",
+                // "alpha-goerli2"
+                // "--fork-block", <VALUE>
+                //
+                // *Chain ID*
+                // "--chain-id", <VALUE>
+                //
+                // *Gas price*
+                // "--gas-price", <VALUE>
+            ]
+        },
+        hardhat: {}
+    }
+};
+
+export default config;
